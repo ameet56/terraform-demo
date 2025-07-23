fef7dc1597f2d63baed5a488d4d397a86279fa46
ameet56 <amitkryash742004@gmail.com>
2025-07-23 20:23:21 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # This script starts the Apache web server (httpd)
-
-# Ensure httpd is installed (it should be from the EC2 user data, but this is a safeguard)
-yum install -y httpd
-yum enable httpd
-
-# Start the web server
-yum start httpd
+service httpd start
+chkconfig httpd on
